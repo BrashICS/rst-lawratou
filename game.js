e36864a45447ad6243316a68481afc36991d6fec
Laouratou Diallo <laouratou.diallo@stu.ocsb.ca>
2024-06-14 08:45:40 -0800

    pushing to get hub

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
-'use strict';
 
-document.getElementById("cardB").addEventListener("click", flip)
 
-function flip(){
+let timer;
+let ele = document.getElementById('time_div')
 
+
+let sec = 0;
+let min = 0;
+timer = setInterval(()=>{
+  ele.innerHTML = min +':'+sec;
+  sec ++;
+if (sec>= 60){
+  sec = 0;
+  min+= 1
 }
+
+
+},1000)
+
