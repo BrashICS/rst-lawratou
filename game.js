ce99c0ea063cf3eabd70e4f664e12034cfc38635
Laouratou Diallo <laouratou.diallo@stu.ocsb.ca>
2024-06-18 07:08:17 -0800

    Final submission RST

--- game.js
+++ game.js
@@ -12,8 +12,44 @@ timer = setInterval(()=>{
 if (sec>= 60){
   sec = 0;
   min+= 1
+}},1000)
+
+
+let moves = 0;
+let flippedCards = [];
+let allmatches =8
+
+function flipCard(card) {
+  if (!card.classList.contains('flipped') && flippedCards.length < 2) {
+    card.classList.add('flipped');
+    flippedCards.push(card);
+ if (flippedCards.length == 2) {
+      moves++;
+      document.getElementById('numb').textContent = moves;
+
+  setTimeout(() => {
+        checkMatch();
+        flippedCards = [];
+      }, 1000);
+    }
+  }
+}
+
+function checkMatch() {
+  if (flippedCards[0].querySelector('.front').src == flippedCards[1].querySelector('.front').src) {
+    flippedCards.forEach(card => {
+      card.removeEventListener('click', flipCard);
+    });
+  } else {
+    flippedCards.forEach(card => {
+      card.classList.remove('flipped');
+    });
+  }
 }
 
 
-},1000)
+function match() {
+if ( checkMatch == allmatches)
+  console.log("All matches found")
+}
 
